46af991237cefbdc8c6b89e1ea51b8a2137dd76c
IcaHuang <ica.huang@qq.com>
2021-12-08 15:53:18 +0800

    Add interface "iterable<String>" in LinkedStackOfStrings.java

--- StackAndQueue/src/LinkedStackOfStrings.java
+++ StackAndQueue/src/LinkedStackOfStrings.java
@@ -1,6 +1,7 @@
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class LinkedStackOfStrings {
+public class LinkedStackOfStrings implements Iterable<String> {
     private Node first;
     private int n;
 
@@ -21,9 +22,9 @@ public class LinkedStackOfStrings {
 //            head = newNode;
 //        }
 //        else {
-            Node oldNode = first;
-            newNode.next = oldNode;
-            first = newNode;
+        Node oldNode = first;
+        newNode.next = oldNode;
+        first = newNode;
 //        }
         n++;
     }
@@ -48,28 +49,53 @@ public class LinkedStackOfStrings {
         return n == 0;
     }
 
-    public static void main(String[] args) {
-	    LinkedStackOfStrings stack = new LinkedStackOfStrings();
-	    int length = 20;
-	    for (int i = 0; i < length; i++) {
-	        stack.push(Integer.toString(i));
-	    }
-	    for (int i = 0; i < length - 2; i++) {
-	        System.out.print(stack.pop() + " ");
+    public Iterator<String> iterator() { return new LinkedIterator(); }
+
+    private class LinkedIterator implements Iterator<String> {
+        private Node current = first;
+
+        public boolean hasNext() { return current != null; }
+        public String next()
+        {
+            String item = current.item;
+            current = current.next;
+            return item;
         }
+    }
+
+    public static void main(String[] args) {
+        LinkedStackOfStrings stack = new LinkedStackOfStrings();
+        int length = 20;
+        for (int i = 0; i < length; i++) {
+            stack.push(Integer.toString(i));
+        }
+
+        System.out.println("------iterable test--------");
+        for (String s : stack) {
+            System.out.print(s + " ");
+        }
+        System.out.println("\n----------------------\n");
+
+        System.out.println("------pop test--------");
+        for (int i = 0; i < length - 2; i++) {
+            System.out.print(stack.pop() + " ");
+        }
+        System.out.println("\n----------------------\n");
+
+        System.out.println("The size of stack: " + stack.size());
+        System.out.println("Is empty? " + stack.isEmpty());
+
+        stack.push("100");
+        stack.push("200");
+        System.out.print(stack.pop() + " ");
+        System.out.print(stack.pop() + " ");
+        System.out.print(stack.pop() + " ");
+        System.out.print(stack.pop() + " ");
         System.out.println();
         System.out.println("The size of stack: " + stack.size());
         System.out.println("Is empty? " + stack.isEmpty());
 
-	    stack.push("100");
-	    stack.push("200");
-	    System.out.print(stack.pop() + " ");
-        System.out.print(stack.pop() + " ");
-        System.out.print(stack.pop() + " ");
-        System.out.print(stack.pop() + " ");
-        System.out.println();
-        System.out.println("The size of stack: " + stack.size());
-        System.out.println("Is empty? " + stack.isEmpty());
+        // this line will throw java.util.NoSuchElementException
         System.out.print(stack.pop() + " ");
 
     }
